3439df243d8bda278fc4be4ed6be83d7891e9d46
TumDraco <35955719+TumDraco@users.noreply.github.com>
2018-01-31 14:59:34 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -15,7 +15,7 @@ if (!is_null($events['events'])) {
 			if (strpos($event['message']['text'], '@BOT') !== false) {
 
 				// Get text sent
-				$text = 'แกนั่นแหละ'.$event['message']['text'];
+				$text = 'แกนั่นแหละ'.str_replace('@BOT','',$event['message']['text']);
 				// Get replyToken
 				$replyToken = $event['replyToken'];
 				// Build message to reply back
