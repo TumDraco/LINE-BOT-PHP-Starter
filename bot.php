f67580138e2748f0ac128afc494d24e59e59c79c
TumDraco <35955719+TumDraco@users.noreply.github.com>
2018-01-31 11:58:07 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -19,9 +19,15 @@ if (!is_null($events['events'])) {
 				// Get replyToken
 				$replyToken = $event['replyToken'];
 				// Build message to reply back
+				$action = [
+					'type' => 'postback',
+					'label' => 'test',
+					'data' => $text
+				];
 				$messages = [
-					'type' => 'text',
-					'text' => $text
+					'type' => 'buttons',
+					'text' => 'test',
+					'actions' => $action
 				];
 
 				// Make a POST Request to Messaging API to reply to sender
