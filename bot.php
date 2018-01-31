904c1367e7882befe5d22ad1c2864e89150b7b0f
TumDraco <35955719+TumDraco@users.noreply.github.com>
2018-01-31 15:05:16 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -12,7 +12,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			
-			if (strpos($event['message']['text'], '@BOT') !== false) {
+			if (strpos(strtoupper($event['message']['text']), '@BOT') !== false) {
 
 				// Get text sent
 				$text = 'แกนั่นแหละ'.str_replace('@BOT','',$event['message']['text']);
